cd4b70ba941c5fcb74416fe955270bc44e528134
Tusharshrivastav <ts3657@gmail.com>
2021-08-28 15:24:59 +0530

    add nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,12 @@
 import "./App.css";
 import { Container } from "react-bootstrap";
-
+import Topbar from "./components/Topbar";
+import { BrowserRouter as Router } from "react-router-dom";
 function App() {
   return (
-    <>
-      <Container style={{ marginTop: "200px" }}>
-        <h4 className="text-center"> Welcome To TechInfo YT Channel</h4>
-        <h1 className="text-center text-white bg-warning p-2 mt-4">
-          Online Pizza Project With Mern Stack
-        </h1>
-        <h2 className="m-4 text-center">
-          follow on Instagram :
-          <strong style={{ color: "red" }}> @technical_update</strong>
-        </h2>
-        <h1>
-          Like Video, comment your queries and dont forget to subscribe channel
-        </h1>
-      </Container>
-    </>
+    <Router>
+      <Topbar />
+    </Router>
   );
 }
 
---
+++ client/src/components/Topbar.jsx
@@ -0,0 +1,33 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import { MdLocalOffer } from "react-icons/md";
+const Topbar = () => {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container fluid>
+          <MdLocalOffer className="text-warning" />
+          <h6 className="text-light">
+            &nbsp;&nbsp; Free Home Delivery on Order Above 500/- Rupess
+          </h6>
+          <Nav className="ms-auto">
+            <LinkContainer to="/" activeClassName>
+              <Nav.Link>Home</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/about" activeClassName>
+              <Nav.Link>About</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/contact" activeClassName>
+              <Nav.Link>Contact Us</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/policy" activeClassName>
+              <Nav.Link>Terms and policy</Nav.Link>
+            </LinkContainer>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
+
+export default Topbar;
