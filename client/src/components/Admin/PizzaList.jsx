fa57b15210cbda4ba1e32a63253f9ed9aa6c369a
Tusharshrivastav <ts3657@gmail.com>
2021-09-29 08:57:24 +0530

    added more feature in addmin deshboard

--- client/src/components/Admin/PizzaList.jsx
+++ client/src/components/Admin/PizzaList.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
-
+import { Table } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { getAllPizzas } from "../../redux/actions/PizzaAction";
+import Loader from "../Loader";
+import Error from "../Error";
+import { FiEdit } from "react-icons/fi";
+import { AiFillDelete } from "react-icons/ai";
 const PizzaList = () => {
-    return (
+  const dispatch = useDispatch();
+  const pizzasState = useSelector((state) => state.getAllPizzaReducer);
+  const { loading, error, pizzas } = pizzasState;
+  useEffect(() => {
+    dispatch(getAllPizzas());
+  }, [dispatch]);
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Error error={error} />
+      ) : (
         <div>
-            pizzalist
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>S/n</th>
+                <th>Pizza Name</th>
+                <th>Prices</th>
+                <th>Category</th>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {pizzas &&
+                pizzas.map((pizza) => (
+                  <tr>
+                    <td>
+                      <img
+                        src={pizza.img}
+                        alt={pizza.name}
+                        width="100px"
+                        height="100px"
+                      />
+                    </td>
+                    <td>{pizza.name}</td>
+                    <td>
+                      Small : {pizza.prices[0]["small"]}
+                      <br />
+                      Medium : {pizza.prices[0]["medium"]}
+                      <br />
+                      Large : {pizza.prices[0]["large"]}
+                    </td>
+                    <td>{pizza.category}</td>
+                    <td>
+                      <FiEdit /> &nbsp;
+                      <AiFillDelete />
+                    </td>
+                  </tr>
+                ))}
+            </tbody>
+          </Table>
         </div>
-    )
-}
+      )}
+    </>
+  );
+};
 
-export default PizzaList
+export default PizzaList;
--- client/src/screens/AdminScreen.jsx
+++ client/src/screens/AdminScreen.jsx
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Row, Col, Container, Button, ButtonGroup } from "react-bootstrap";
 import { Switch, Route } from "react-router-dom";
 import UserList from "../components/Admin/UsersList";
 import PizzaList from "../components/Admin/PizzaList";
 import AddnewPizza from "../components/Admin/AddNewPizza";
 import OrderList from "../components/Admin/OrderList";
+import { useDispatch, useSelector } from "react-redux";
+const AdminScreen = ({ history }) => {
+  const { currentUser } = useSelector((state) => state.userLoginReducer);
 
-const AdminScreen = ({history}) => {
+  // useEffect(() => {
+  //   if(localStorage.getItem("currentUser") === null || !currentUser.isAdmin) {
+  //     window.location.href = "/";
+  //   }
+  // }, []);
   return (
     <>
       <Container>
         <Row>
           <h1 className="text-center bg-dark text-light p-2">Admin Panel</h1>
-          <Col md={4}>
+          <Col md={3}>
             <ButtonGroup vertical style={{ minHeight: "400px" }}>
-              <Button>All Users</Button>
-              <Button style={{ marginTop: "5px" }}> All Pizzas</Button>
-              <Button style={{ marginTop: "5px" }}>Add New Pizza</Button>
-              <Button style={{ marginTop: "5px" }}>All Orders</Button>
+              <Button onClick={() => history.push("/admin")}>All Users</Button>
+              <Button
+                onClick={() => history.push("/admin/pizzalist")}
+                style={{ marginTop: "5px" }}
+              >
+                {" "}
+                All Pizzas
+              </Button>
+              <Button
+                onClick={() => history.push("/admin/addnewpizza")}
+                style={{ marginTop: "5px" }}
+              >
+                Add New Pizza
+              </Button>
+              <Button
+                onClick={() => history.push("/admin/orderlist")}
+                style={{ marginTop: "5px" }}
+              >
+                All Orders
+              </Button>
             </ButtonGroup>
           </Col>
-          <Col md={8}>
+          <Col md={9}>
             <Switch>
               <Route path="/admin" component={UserList} exact />
               <Route path="/admin/pizzalist" component={PizzaList} exact />
